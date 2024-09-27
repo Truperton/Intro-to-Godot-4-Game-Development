b31288dbcf9fda14cff4bb4b30c5dd07866e803e
Truperton <58820346+Truperton@users.noreply.github.com>
2024-09-27 16:31:57 +0100

    Update CountdownChallenge.gd
    Corrected the formatting to correct standard for the language.

--- Scripts/CountdownChallenge.gd
+++ Scripts/CountdownChallenge.gd
@@ -1,16 +1,16 @@
 extends Node
 
-var timeLeft : float = 5
+var time_left : float = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(timeLeft)
+	print(time_left)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if timeLeft >= 0:
-		timeLeft -= delta
-		print(timeLeft)
+	if time_left >= 0:
+		time_left -= delta
+		print(time_left)
 	else:
 		print("Time Over")
